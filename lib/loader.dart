ec050e568a752ea15987db4ebe9960d9b7526af8
davinci150 <79250693+davinci150@users.noreply.github.com>
2021-03-04 14:10:09 +0300

    add preload

---
+++ lib/loader.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:mynewproject/home_screen.dart';
+
+class LoaderScreen extends StatefulWidget {
+  //LoaderScreen(Alignment center);
+  LoaderScreen();
+
+  @override
+  _LoaderState createState() => _LoaderState();
+}
+
+class _LoaderState extends State<LoaderScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(milliseconds: 5000), () {
+      //Navigator.pushNamed(context, HomeScreen());
+      //Navigator.of(context).pushNamed(HomeScreen);
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      //padding: EdgeInsets.only(left: 130, right: 130, top: 356),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 140),
+        child: Container(
+          width: 210,
+          child: Column(
+            //width: MediaQuery.of(context).size.width * 0.9,
+            mainAxisAlignment: MainAxisAlignment.center,
+            //mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.asset(
+                'assets/ex.png',
+                width: 47,
+                height: 82,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Image.asset(
+                'assets/exciter1.png',
+                width: 210,
+                height: 75,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const LinearProgressIndicator(
+                backgroundColor: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
-import 'package:flutter/material.dart';\
+import 'package:flutter/material.dart';
+import 'loader.dart';
+//import 'package:static_analyze_av/static_analyze_av.dart';
+//import 'home_screen.dart';
 
-class 123
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: ('Recent'),
+      home: LoaderScreen(),
+    );
+  }
+}
