98361ae0e016beab5f8aaa7909f64fe2d69726af
davinci150 <davinci150cc@gmail.com>
2021-04-06 14:49:19 +0300

    add start/end value for video screen

--- lib/module/processing/video/video_screen.dart
+++ lib/module/processing/video/video_screen.dart
@@ -6,15 +6,16 @@ import '../widget/list_video_widget.dart';
 
 class VideoScreen extends StatefulWidget {
   const VideoScreen({
-    Key key,@required this.showOrHideBottomToolbar,
+    Key key,
+    @required this.showOrHideBottomToolbar,
   }) : super(key: key);
-final void Function(bool isBottomToolvarVisible) showOrHideBottomToolbar;
+  final void Function(bool isBottomToolvarVisible) showOrHideBottomToolbar;
   @override
   _VideoScreen createState() => _VideoScreen();
 }
 
-var _valueSize = 0.0;
-var _valueOpacity = 0.0;
+/*var _valueSize = 0.0;
+var _valueOpacity = 0.0;*/
 
 class _VideoScreen extends State<VideoScreen> {
   FilterVideoButtonModel selectedFilterVideo;
@@ -24,24 +25,50 @@ class _VideoScreen extends State<VideoScreen> {
       children: [
         Expanded(
           child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-           Visibility(
-            visible: selectedFilterVideo !=null,
-                      child: SlidersVideoWidget(
-                        editText: selectedFilterVideo?.name ?? '',
-                        onAsseptDeclineButtonClick: (bool isAssept) {
-                          setState(() {
-                            selectedFilterVideo= null;
-                            widget.showOrHideBottomToolbar(true);
-                          });
-                        },
-                      ),
-          ),
-          ]),),
+            Visibility(
+              visible: selectedFilterVideo != null,
+              child: Column(
+                children: [
+                  SlidersVideoWidget(
+                    endValue: 200,
+                    startValue: 0,
+                    defaultValue: 100,
+                    sliderValueListener: (double value) {
+                      debugPrint('slider value = $value');
+                    },
+                    editText: selectedFilterVideo?.name ?? '',
+                    onAsseptDeclineButtonClick: (bool isAssept) {
+                      setState(() {
+                        selectedFilterVideo = null;
+                        widget.showOrHideBottomToolbar(true);
+                      });
+                    },
+                  ),
+                  SlidersVideoOpacityWidget(
+                    endValue: 200,
+                    startValue: 0,
+                    defaultValue: 100,
+                    sliderValueListener: (double value) {
+                      debugPrint('slider value = $value');
+                    },
+                    editText: selectedFilterVideo?.name ?? '',
+                    onAsseptDeclineButtonClick: (bool isAssept) {
+                      setState(() {
+                        selectedFilterVideo = null;
+                        widget.showOrHideBottomToolbar(true);
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
         const SizedBox(
           height: 16,
         ),
         if (selectedFilterVideo == null)
-         SizedBox(
+          SizedBox(
             height: 72,
             child: Padding(
                 padding: const EdgeInsets.only(left: 20, right: 20),
@@ -54,7 +81,8 @@ class _VideoScreen extends State<VideoScreen> {
                   },
                 )),
           )
-        else const SizedBox.shrink(),
+        else
+          const SizedBox.shrink(),
         const SizedBox(
           height: 16,
         )
@@ -62,131 +90,184 @@ class _VideoScreen extends State<VideoScreen> {
     );
   }
 }
+
 class SlidersVideoWidget extends StatefulWidget {
   const SlidersVideoWidget(
-      {Key key, this.onAsseptDeclineButtonClick, this.editText})
+      {Key key,
+      @required this.onAsseptDeclineButtonClick,
+      @required this.editText,
+      @required this.sliderValueListener,
+      @required this.startValue,
+      @required this.endValue,
+      @required this.defaultValue})
       : super(key: key);
-      final String editText;
-final void Function(bool isAssept) onAsseptDeclineButtonClick;
-
+  final String editText;
+  final void Function(bool isAssept) onAsseptDeclineButtonClick;
+  final void Function(double value) sliderValueListener;
+  final double startValue;
+  final double endValue;
+  final double defaultValue;
   @override
   _SlidersVideoWidgetState createState() => _SlidersVideoWidgetState();
 }
 
 class _SlidersVideoWidgetState extends State<SlidersVideoWidget> {
+  double defaultValue;
+  @override
+  void initState() {
+    defaultValue = widget.defaultValue;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 49),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Size',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontFamily: AppFonts.sfPro,
-                              color: Theme.of(context).primaryIconTheme.color,
-                            ),
-                          ),
-                          const Spacer(flex: 15),
-                          Text(
-                            _valueSize.round().toString(),
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontFamily: AppFonts.sfPro,
-                              color: Theme.of(context).primaryIconTheme.color,
-                            ),
-                          ),
-                          const Spacer(
-                            flex: 17,
-                          )
-                        ],
-                      ),
-                    ),
-      
-    
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 29),
-                  child: SliderTheme(
-                    data: const SliderThemeData(
-                      //trackShape: CustomTrackShape(),
-                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 13),
-                    ),
-                    child: Slider(
-                      value: _valueSize,
-                      activeColor: const Color(0xFF2B83EC),
-                      inactiveColor:
-                          WidgetsBinding.instance.window.platformBrightness ==
-                                  Brightness.light
-                              ? const Color(0xFFEAEAEA)
-                              : const Color(0xFF313131),
-                      min: -50,
-                      max: 50,
-                      divisions: 100,
-                      onChanged: (double value) {
-                        setState(() {
-                          _valueSize = value;
-                        });
-                      },
-                    ),
-                  ),
+          padding: const EdgeInsets.symmetric(horizontal: 49),
+          child: Row(
+            children: [
+              Text(
+                'Size',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: AppFonts.sfPro,
+                  color: Theme.of(context).primaryIconTheme.color,
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 49),
-                  child: Row(
-                    children: [
-                      Text(
-                        'Opacity',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontFamily: AppFonts.sfPro,
-                          color: Theme.of(context).primaryIconTheme.color,
-                        ),
-                      ),
-                      const Spacer(flex: 12),
-                      Text(
-                        _valueOpacity.round().toString(),
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontFamily: AppFonts.sfPro,
-                          color: Theme.of(context).primaryIconTheme.color,
-                        ),
-                      ),
-                      const Spacer(
-                        flex: 18,
-                      ),
-                    ],
-                  ),
+              ),
+              const Spacer(flex: 15),
+              Text(
+                defaultValue.toString(),
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: AppFonts.sfPro,
+                  color: Theme.of(context).primaryIconTheme.color,
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 29),
-                  child: SliderTheme(
-                    data: const SliderThemeData(
-                      //trackShape: CustomTrackShape(),
-                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 13),
-                    ),
-                    child: Slider(
-                      value: _valueOpacity,
-                      activeColor: const Color(0xFF2B83EC),
-                      inactiveColor:
-                          WidgetsBinding.instance.window.platformBrightness ==
-                                  Brightness.light
-                              ? const Color(0xFFEAEAEA)
-                              : const Color(0xFF313131),
-                      min: -50,
-                      max: 50,
-                      divisions: 100,
-                      onChanged: (double value) {
-                        setState(() {
-                          _valueOpacity = value;
-                        });
-                      },
-                    ),
-                  ),
+              ),
+              const Spacer(
+                flex: 17,
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 29),
+          child: SliderTheme(
+            data: const SliderThemeData(
+              //trackShape: CustomTrackShape(),
+              thumbShape: RoundSliderThumbShape(enabledThumbRadius: 13),
+            ),
+            child: Slider(
+              value: defaultValue,
+              activeColor: const Color(0xFF2B83EC),
+              inactiveColor:
+                  WidgetsBinding.instance.window.platformBrightness ==
+                          Brightness.light
+                      ? const Color(0xFFEAEAEA)
+                      : const Color(0xFF313131),
+              min: widget.startValue,
+              max: widget.endValue,
+              divisions: 100,
+              onChanged: (double value) {
+                widget.sliderValueListener(value);
+                setState(() {
+                  defaultValue = value;
+                });
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SlidersVideoOpacityWidget extends StatefulWidget {
+  const SlidersVideoOpacityWidget(
+      {Key key,
+      @required this.onAsseptDeclineButtonClick,
+      @required this.editText,
+      @required this.sliderValueListener,
+      @required this.startValue,
+      @required this.endValue,
+      @required this.defaultValue})
+      : super(key: key);
+  final String editText;
+  final void Function(bool isAssept) onAsseptDeclineButtonClick;
+  final void Function(double value) sliderValueListener;
+  final double startValue;
+  final double endValue;
+  final double defaultValue;
+  @override
+  _SlidersVideoOpacityWidgetState createState() => _SlidersVideoOpacityWidgetState();
+}
+
+class _SlidersVideoOpacityWidgetState extends State<SlidersVideoOpacityWidget> {
+  double defaultValue;
+  @override
+  void initState() {
+    defaultValue = widget.defaultValue;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 49),
+          child: Row(
+            children: [
+              Text(
+                'Opacity',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: AppFonts.sfPro,
+                  color: Theme.of(context).primaryIconTheme.color,
                 ),
-                const SizedBox(height: 0),
+              ),
+              const Spacer(flex: 15),
+              Text(
+                defaultValue.toString(),
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: AppFonts.sfPro,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+              ),
+              const Spacer(
+                flex: 17,
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 29),
+          child: SliderTheme(
+            data: const SliderThemeData(
+              //trackShape: CustomTrackShape(),
+              thumbShape: RoundSliderThumbShape(enabledThumbRadius: 13),
+            ),
+            child: Slider(
+              value: defaultValue,
+              activeColor: const Color(0xFF2B83EC),
+              inactiveColor:
+                  WidgetsBinding.instance.window.platformBrightness ==
+                          Brightness.light
+                      ? const Color(0xFFEAEAEA)
+                      : const Color(0xFF313131),
+              min: widget.startValue,
+              max: widget.endValue,
+              divisions: 100,
+              onChanged: (double value) {
+                widget.sliderValueListener(value);
+                setState(() {
+                  defaultValue = value;
+                });
+              },
+            ),
+          ),
+        ),const SizedBox(height: 0),
                  Padding(
                    padding: const EdgeInsets.symmetric(horizontal: 36),
                    child: EditTextWidget(
@@ -194,5 +275,6 @@ class _SlidersVideoWidgetState extends State<SlidersVideoWidget> {
           editText: widget.editText,
                ),
                  ) ],);
+     
   }
-    }
+}
